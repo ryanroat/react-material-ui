f4509fbd4c937decb972aace15d64426667d4527
Ryan Roat <ryanroat@gmail.com>
2021-07-02 15:47:04 -0400

    Icon Button demo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from '@material-ui/core/Button'
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward'
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Button
+          startIcon={<ArrowForwardIcon />}
           variant="contained"
           color="primary"
           // style={{
           //   fontSize: 26
           // }}
+          endIcon={<ArrowBackIcon />}
           >
           Click Me
         </Button>
